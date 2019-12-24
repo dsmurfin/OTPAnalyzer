7bc19af2bbeb91c1faea450bde74ee859cc73eb5
Daniel Murfin <dan@dsmurfin.co.uk>
2019-12-24 15:51:42 +0000

    Initial Commit

---
+++ OTPAnalyzer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OTPAnalyzer
+//
+//  Created by Daniel Murfin on 24/12/2019.
+//  Copyright © 2019 Daniel Murfin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
